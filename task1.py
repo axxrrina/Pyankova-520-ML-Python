e27a3d64d93326a3a5464d2b4fe9df532d3f0db5
axxrrina <106758405+axxrrina@users.noreply.github.com>
2022-11-04 02:53:50 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+#1. Welcome to Python
+#Дано: имя и фамилия.
+
+#Задание: написать программу, которая будет приветствовать нового человека в мире Python.
+#Текст приветсвия: Hello NAME SURNAME! You just delved into Python. Great start!
+print('Enter your name>>')
+name = input()
+print('Hello',name,'! You just delved into Python. Great start!')
